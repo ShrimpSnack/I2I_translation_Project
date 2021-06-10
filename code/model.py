c5c8a5c385929c0a921e561d60396c47e00aba0d
dnwjddl <woojung980305@gmail.com>
2021-06-10 21:09:47 +0900

    Update model.py

--- code/model.py
+++ code/model.py
@@ -16,9 +16,25 @@ S = stride size
 P = padding size
 IN = instance Normalization
 '''
+
+'''
+# x와 c를 왜 concat해주는지 의문
+'''
 class Generator(nn.Module):
   def __init__(self):
     super(Generator, self).__init__()
+    layers = []
     
-  def forward(self, x, c):
+    
+    
+    self.main = nn.Sequential(*layers)
+    
+  def forward(self, x, c): 
+    # real image x 와 Target domain c
+    # x는 128x128크기, batchsize 16개, dimension 3 -> [16,3, 128, 128]
+    # c는 16x7의 사이즈 (16개의 이미지에 대한 label 7개)
+    c = c.view(c.size(0), c.size(1), 1, 1) # [16, 7, 1, 1]
+    c = c.repeat(1,1, x.size(2), x.size(3)) # [16, 7, 128, 128]
+    x = torch.cat([x,c], dim =1) # [16, 10, 128, 128] 
+    return self.main(x)
     
